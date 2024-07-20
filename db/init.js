32f63b35953965b21df4f11dc5933c2931c30297
unknown <juanvalera2001@gmail.com>
2024-07-20 16:04:54 -0400

    Ultimos cambios

--- db/init.js
+++ db/init.js
@@ -1,32 +1,22 @@
 // db/init.js
-const Database = require('better-sqlite3');
-const db = new Database('./db/database.db', { verbose: console.log });
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/database.db');
 
-try {
-  // Crear la tabla 'categorias' si no existe
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS categorias (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      categoria TEXT NOT NULL
-    )
-  `);
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS categorias (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    categoria TEXT NOT NULL
+  )`);
 
-  // Crear la tabla 'productos' si no existe
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS productos (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      codigo TEXT NOT NULL,
-      producto TEXT NOT NULL,
-      categoria_id INTEGER,
-      existencia_actual INTEGER,
-      precio REAL,
-      FOREIGN KEY (categoria_id) REFERENCES categorias(id)
-    )
-  `);
-  
-  console.log("Tablas creadas o ya existen.");
-} catch (error) {
-  console.error("Error al crear las tablas:", error);
-} finally {
-  db.close();
-}
+  db.run(`CREATE TABLE IF NOT EXISTS productos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    codigo TEXT NOT NULL,
+    producto TEXT NOT NULL,
+    categoria_id INTEGER,
+    existencia_actual INTEGER,
+    precio REAL,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+  )`);
+});
+
+db.close();
